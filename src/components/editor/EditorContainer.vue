946d607cf47ec25b5fa12828ab7ea6ed089ab4c9
liudi6 <297114195@qq.com>
2026-04-20 13:54:44 +0800

    refactor: wire editor onChange to updateTabContent

--- src/components/editor/EditorContainer.vue
+++ src/components/editor/EditorContainer.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import { useEditorManager } from '../../core/editor/EditorManager'
+import { activeTabId, updateTabContent } from '../../core/stores/tabStore'
 import EditorCore from './EditorCore.vue'
 import SourceView from './SourceView.vue'
 import SplitView from './SplitView.vue'
 
 const { mode, content } = useEditorManager()
+
+function handleChange(newContent: string) {
+  const tabId = activeTabId.value
+  if (tabId) {
+    updateTabContent(tabId, newContent)
+  }
+}
 </script>
 
 <template>
@@ -13,18 +21,21 @@ const { mode, content } = useEditorManager()
     <EditorCore
       v-if="mode === 'wysiwyg'"
       :content="content"
+      @change="handleChange"
     />
 
     <!-- 源码模式 -->
     <SourceView
       v-if="mode === 'source'"
       :content="content"
+      @change="handleChange"
     />
 
     <!-- 分屏模式 -->
     <SplitView
       v-if="mode === 'split'"
       :content="content"
+      @change="handleChange"
     />
   </div>
 </template>
